b4d0d8543879198f034300f4a02b38d5bf239715
Yasmin Aline Amaral <98675367+yasmin-aline@users.noreply.github.com>
2024-04-15 07:58:30 -0300

    Array

--- Main.java
+++ Main.java
@@ -1,15 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package array;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner leitura = new Scanner (System.in);
+        ArrayList<String> nomes = new ArrayList<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Digite um nome");
+        String nome_recebido = leitura.nextLine();
+        nomes.add(nome_recebido);
+
+        for(String nome : nomes){
+            System.out.println(nome);
         }
+        System.out.println(nomes.get(0));
+
     }
 }
